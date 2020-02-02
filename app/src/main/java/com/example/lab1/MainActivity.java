49c8847de7c191e2a0a1fbaf3a7a8280fd738c40
Ben30000 <BMARKOS@wisc.edu>
2020-02-02 17:44:14 -0600

    Test Change 2

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int a = 1;
-
+    int b = 2;
+    int c = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
